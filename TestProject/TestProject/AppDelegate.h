eacbc5f9b08b239a2f441524ef203f471d7e8f76
linlishu8 <linlishu8@163.com>
2017-11-08 16:44:41 +0800

    基本工程

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by 动感超人 on 2017/11/8.
+//  Copyright © 2017年 LiuweiChina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
